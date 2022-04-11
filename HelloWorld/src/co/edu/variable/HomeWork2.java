a0fcb61da0da6472bfe1389933e6968f02be3776
admin <admin@YD2-03>
2022-04-11 17:46:12 +0900

    fin

--- HelloWorld/src/co/edu/variable/HomeWork2.java
+++ HelloWorld/src/co/edu/variable/HomeWork2.java
@@ -1,18 +1,17 @@
 package co.edu.variable;
 
 public class HomeWork2 {
-
 	public static void main(String[] args) {
 		// 영수증
 		// 물건의 가격과 현금
 		// [현금] 10000원
 		// [물건가격] 7500원
-		// [부가세] 750원
+		// [부가세] 682원
 		// [잔돈] 2500원
 		int money = 10000;
 		int pur = 7500;
-		int v = pur / 10;
+		double v = pur / 1.1 * 0.1;
 		int ch = money - pur;
-		System.out.printf("[현금] : %d원, [물건가격] : %d원, [부가세] : %d원, [잔돈] : %d원", money, pur, v, ch);
+		System.out.printf("[현금] : %d원, [물건가격] : %d원, [부가세] : %.0f원, [잔돈] : %d원", money, pur, v, ch);
 	}
 }
