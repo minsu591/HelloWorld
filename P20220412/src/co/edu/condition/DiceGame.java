69a58fdd6612f2894ae7ddf5748086493ab56ccb
admin <admin@YD2-03>
2022-04-12 13:54:13 +0900

    220412

--- P20220412/src/co/edu/condition/DiceGame.java
+++ P20220412/src/co/edu/condition/DiceGame.java
@@ -4,7 +4,7 @@ public class DiceGame {
 	public static void main(String[] args) {
 		//임의의 값 범위 (1~6 : 주사위의 면의 갯수)
 		//주사위 맞추기 게임
-		int diceNum = (int) (Math.random()*6+1);
+		int diceNum = (int) (Math.random()*6+1); 
 		
 		
 	}
